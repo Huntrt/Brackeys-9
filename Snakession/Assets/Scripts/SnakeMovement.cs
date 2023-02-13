b51cc8901c3de760152b545a99d392ab1d869e15
Huntrt <huntrt.business@outlook.com>
2023-02-13 12:34:12 +0700

    Replace follow mouse with snake movement that support turn speed

---
+++ Snakession/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+	public float moveSpeed;
+	public float rotateSpeed;
+	[SerializeField] Rigidbody2D rb;
+	Vector2 mousePos;
+	Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+		//Get mouse position
+       	mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+	void FixedUpdate()
+	{
+		//If has reached 0.1 of an step toward mouse
+		if(Vector2.Distance(rb.position, mousePos) <= moveSpeed/80)
+		{
+			//Stop moving
+			return;
+		}
+		//Make object slowly facing toward mouse position
+		transform.up = Vector3.Slerp(transform.up, (mousePos - rb.position), rotateSpeed * Time.deltaTime);
+		//Get velocity by multiply speed with where object facing
+	  	Vector2 velocity = transform.up * moveSpeed;
+		//Moving the player toward velocity has get
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
