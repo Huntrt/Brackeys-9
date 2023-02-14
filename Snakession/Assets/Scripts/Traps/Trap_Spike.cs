cbe1ad632f9af5bd863f46dcccc70d6e6a146f45
Huntrt <huntrt.business@outlook.com>
2023-02-15 00:15:44 +0700

    Simple spike trap that hurt the snake once when collide with

---
+++ Snakession/Assets/Scripts/Traps/Trap_Spike.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Trap_Spike : MonoBehaviour
+{
+	public int damage;
+	[SerializeField] Collider2D trapCollider;
+	Snake snake;
+	bool collidedWithSnake;
+
+	void OnEnable()
+	{
+		snake = Snake.i;
+	}
+
+    void OnTriggerEnter2D(Collider2D other) 
+	{
+		//If this the first eneter of spike collide with player 
+		if(other.CompareTag("Player") && !collidedWithSnake)
+		{
+			//Hurt the player
+			snake.Hurt(damage);
+			collidedWithSnake = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) 
+	{
+		//If exit player collider 
+		if(other.CompareTag("Player"))
+		{
+			collidedWithSnake = false;
+		}
+		
+	}
+}
