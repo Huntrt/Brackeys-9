7e1c0748ecf7e90f18a1e63958fc81793fee5803
Huntrt <huntrt.business@outlook.com>
2023-02-15 19:59:32 +0700

    Food will depsawn when it no longer fresh

--- Snakession/Assets/Scripts/Foods/Foods.cs
+++ Snakession/Assets/Scripts/Foods/Foods.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 public class Foods : MonoBehaviour
 {
     public int feed;
+	public float fresh;
 	public Vector2 spawnCoord;
 	public bool isVegan;
+
+	void OnEnable()
+	{
+		//Automaticly pluck this food when it no longer fresh
+		Invoke("Pluck", fresh);
+	}
 
 	void OnCollisionEnter2D(Collision2D other) 
 	{
 		if(other.collider.CompareTag("Player"))
 		{
+			//Snake eat this food
 			Snake.i.Eat(feed, isVegan);
-			Consumed();
+			//Pluck the food
+			Pluck();
 		}
 	}
 
-	protected virtual void Consumed()
+	protected virtual void Pluck()
 	{
 		//Pluck object where this food got spawn (also destroy)
 		Map.i.PluckObject(spawnCoord);
