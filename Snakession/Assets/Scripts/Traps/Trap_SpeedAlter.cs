d33fdf336f43f51cae050a75b3e09ac182c0383d
Huntrt <huntrt.business@outlook.com>
2023-02-17 15:22:16 +0700

    An trap that will later snake speed when step on

---
+++ Snakession/Assets/Scripts/Traps/Trap_SpeedAlter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Trap_SpeedAlter : MonoBehaviour
+{
+	public string sourceName;
+    public float speedAlterAdditive;
+	public float decayDuration;
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//If player stay in the trap
+		if(other.CompareTag("Player"))
+		{
+			//Apply speed alter that slowly decay to move speed boost
+			Snake.i.mod.moveSpeedBoost.AddBoost(speedAlterAdditive, decayDuration, sourceName);
+		}
+	}
+}
