4ee8416fab5a1239097e19c2ea7625330180e406
Huntrt <huntrt.business@outlook.com>
2023-02-19 05:26:58 +0700

    Create exploding effect for each body part when game over

--- Snakession/Assets/Scripts/GameOver.cs
+++ Snakession/Assets/Scripts/GameOver.cs
@@ -19,6 +19,7 @@ public class GameOver : MonoBehaviour
 
 	[SerializeField] GameObject gameOverPanel;
     [SerializeField] TextMeshProUGUI scoreText, levelText, maxHpAccuText, foodEatenText;
+	[SerializeField] GameObject overBodyParticle;
 
 	public void GameIsOver()
 	{
@@ -29,5 +30,14 @@ public class GameOver : MonoBehaviour
 		levelText.text = "<align=left>Level<line-height=0>\n<align=right>"+Map.i.level+"<line-height=1em>";
 		maxHpAccuText.text = "<align=left>Max HP Accumulated<line-height=0>\n<align=right>"+Snake.i.totalMaxHPGained+"<line-height=1em>";
 		foodEatenText.text = "<align=left>Food Eaten<line-height=0>\n<align=right>"+Snake.i.foodEaten+"<line-height=1em>";
+		
+		//Create over effect for head
+		Instantiate(overBodyParticle, Snake.i.body.head.position, Quaternion.identity);
+		//Go through all the body's line renderer position
+		LineRenderer body = Snake.i.body.line; for (int i = 0; i < body.positionCount; i++)
+		{
+			//Create effect at each body position
+			Instantiate(overBodyParticle, body.GetPosition(i), Quaternion.identity);
+		}
 	}
 }
