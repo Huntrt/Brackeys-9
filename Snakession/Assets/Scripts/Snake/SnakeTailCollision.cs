09a3507b80b5041ed44663190538396a0aad6769
Huntrt <huntrt.business@outlook.com>
2023-02-19 03:43:22 +0700

    Play an effect when tail appear

--- Snakession/Assets/Scripts/Snake/SnakeTailCollision.cs
+++ Snakession/Assets/Scripts/Snake/SnakeTailCollision.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class SnakeTailCollision : MonoBehaviour
 {
+	[SerializeField] ParticleSystem tailAppearEffect;
+
     void OnCollisionEnter2D(Collision2D other) 
 	{
 		//Begin eating tail if collide with the snake's head
 		if(other.collider.CompareTag("Player")) Snake.i.body.EatingTail();
+	}
+
+	void OnEnable()
+	{
+		tailAppearEffect.Play();
 	}
 }
